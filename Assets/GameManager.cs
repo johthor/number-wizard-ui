5f3891a149fa665dc5b04b43fff811d14d128ebe
Johannes Thorn <johannes.thorn@g4t3.de>
2017-04-14 22:54:21 +0200

    Add GameManager to switch between scenes

---
+++ Assets/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public void LoadScene(string scene)
+    {
+        print("Scene load requested for " + scene);
+        SceneManager.LoadScene(scene);
+    }
+
+    public void QuitRequest()
+    {
+        print("Quit game requested");
+    }
+}

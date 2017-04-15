f139a74af08a997bb9fba58ea8a747d32ba74207
Johannes Thorn <johannes.thorn@g4t3.de>
2017-04-15 17:20:42 +0200

    Import code from previous game and wire everything

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    private int minNumber;
+    private int maxNumber;
+
+    private int guess;
+    private int guessesRemaining;
+
+    public Text guessText;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();	
+    }
+
+    public void GuessHigher()
+    {
+        minNumber = guess;
+        MakeAGuess();
+    }
+
+    public void GuessLower()
+    {
+        maxNumber = guess;
+        MakeAGuess();
+
+    }
+
+    private	void StartGame()
+    {
+        minNumber = 1;
+        maxNumber = 1000;
+
+        guessesRemaining = 10;
+
+        MakeAGuess();
+    }
+
+    private void MakeAGuess()
+    {
+        guess =	Random.Range(minNumber, maxNumber + 1);
+        guessText.text = guess.ToString();
+        guessesRemaining -= 1;
+
+        if (guessesRemaining <= 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+}
